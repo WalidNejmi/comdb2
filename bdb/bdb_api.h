51e805687d2fa81581912f033f1178cd9a2a2ec9
Walid Nejmi <wnejmi@bloomberg.net>
2026-09-23 14:51:41 +0000

    txn: classify direct schema-change copy transactions
    Signed-off-by: Walid Nejmi <wnejmi@bloomberg.net>

--- bdb/bdb_api.h
+++ bdb/bdb_api.h
@@ -30,6 +30,7 @@
 
 #include <net.h>
 #include <bb_stdint.h>
+#include <sc_build_id.h>
 
 #include <inttypes.h>
 #include <limits.h>
@@ -2540,4 +2541,12 @@ typedef int (*collect_unused_files_f)(void *args, int lognum, char *filename);
 
 void oldfile_hash_collect(collect_unused_files_f func, void *arg);
 void bdb_tran_set_is_sc_rebuild(tran_type *tran, int is_sc_rebuild);
+void bdb_tran_set_sc_build(tran_type *tran, const sc_build_id_t *build_id);
+int bdb_sc_private_register_files(bdb_state_type *bdb_state,
+                                  const sc_build_id_t *build_id,
+                                  int dta_rebuilt, const int *blob_rebuilt,
+                                  int nblobs, const int *ix_rebuilt, int nix,
+                                  int *nregistered);
+int bdb_sc_private_unregister_build(bdb_state_type *bdb_state,
+                                    const sc_build_id_t *build_id);
 #endif
